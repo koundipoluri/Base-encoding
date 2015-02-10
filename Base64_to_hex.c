d53e27126e34c76bb04bdd1a5e62df97d723ca9a
luffy-93 <koundi.poluri@ymail.com>
2015-02-10 16:05:07 +0530

    Update Base64_to_hex.c

--- Base64_to_hex.c
+++ Base64_to_hex.c
@@ -5,6 +5,11 @@ unsigned char hexchar(int i)
 	unsigned char hexchar_arr[]="0123456789abcdef";
 	return hexchar_arr[i];
 }
+unsigned char base64char(int i)
+{
+	unsigned char base64char_arr[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	return base64char_arr[i];
+}
 int decval_64(unsigned char x)
 {
 	int y=x;
@@ -22,7 +27,17 @@ int decval_64(unsigned char x)
 	if (48 <= y && y <= 57)
 		return y-48+52;
 }
-void Base64_to_hex(unsigned char* in,size_t inlen,unsigned char* out,size_t outlen)
+int decval_16(unsigned char x)
+{
+	int y= x;
+	if(48<=y && y<=57)
+		return y-48;
+	else if(97<=y && y<=102)
+		return y-97+10;
+	else if(65<=y && y<=70)
+		return y-65+10;
+}
+void base64_to_hex(unsigned char* in,size_t inlen,unsigned char* out,size_t outlen)
 {
 	int i=0,j=0,a,b;
 	for(i=0;i<inlen;i++)
@@ -36,13 +51,29 @@ void Base64_to_hex(unsigned char* in,size_t inlen,unsigned char* out,size_t outl
 	}
 
 }
+void hex_to_base64(unsigned char* in,size_t inlen,unsigned char *out,size_t outlen)
+{
+		int i,j=0,a,b,c;
+		for(i=0;i<inlen;i++)
+		{
+			a= decval_16(in[i++]);
+			b= decval_16(in[i++]);
+			c= decval_16(in[i]);
+			
+			out[j++]= base64char((a<<2) + (b>>2));
+			out[j++]= base64char(((b & 3)<<4) + (c));
+		}
+	
+}
 int main()
 {
-	//hard coded values need to be updated!!
 	unsigned char x[]="UKXO5d/Fazj28j4Hutlm/w==";
+	unsigned char u[]= "50a5cee5dfc56b38f6f23e07bad966ff";
 	unsigned char *y = calloc(32,1);
-	
-	Base64_to_hex(x,strlen(x),y,32);
+	unsigned char *v = calloc(24,1);
+	hex_to_base64(u,strlen(u),v,24);
+	base64_to_hex(x,strlen(x),y,32);
+	printf("v string is %s\n",v);
 	printf("y string is %s\n",y);
 	return 0;
 }
